3d74642b71985f5bf744747c0c27cf805c1e619c
JdiraSiham <jdirasiham@gmail.com>
2025-10-16 13:13:48 +0100

    Update Atelier 1 Activité 19.py

--- ATELIER 01/Atelier 1 Activité 19.py
+++ ATELIER 01/Atelier 1 Activité 19.py
@@ -1,6 +1,6 @@
 #Réalisée par : JDIRA Siham 
 
-#Séance 1 Activité 19:
+#Atelier 1 Activité 19:
 
 for i in range(5,0,-1):
     for j in range(5-i):
@@ -13,4 +13,5 @@ for i in range(6):
         print(" ",end="")
     for k in range (i):
         print("*",end=" ")
+
     print("")
