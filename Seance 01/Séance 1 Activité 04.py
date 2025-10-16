20d54d6bef5e944fa303105e6fad78b578412e58
JdiraSiham <jdirasiham@gmail.com>
2025-10-16 13:01:45 +0100

    Delete Seance 01 directory

--- Seance 01/Séance 1 Activité 04.py
+++
@@ -1,8 +0,0 @@
-#Réalisée par : JDIRA Siham 
-
-#Séance 1 Activité 4:
-
-for i in range (4):
-    for j in range(7):
-        print ("*",end=" ")
-    print("")
--- Seance 01/Séance 1 Activité 17.py
+++
@@ -1,8 +0,0 @@
-#Réalisée par : JDIRA Siham 
-
-#Séance 1 Activité 17:
-
-for i in range(6):
-    for j in range(i):
-        print(i-j,end=" ")
-    print("")
