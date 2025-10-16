d6a1913282826bb61d60e125a8dbce990215a517
JdiraSiham <jdirasiham@gmail.com>
2025-10-16 18:47:05 +0100

    Update Atelier 4 Activité 03.py

--- ATELIER 04/Atelier 4 Activité 03/Atelier 4 Activité 03.py
+++ ATELIER 04/Atelier 4 Activité 03/Atelier 4 Activité 03.py
@@ -1,5 +1,6 @@
 import csv
-with open('C:\Users\jdira\Desktop\data.csv', 'r') as file:
+with open('C:\data.csv', 'r') as file:
     csv_reader = csv.reader(file)
     for line in csv_reader:
+
         print(line)
