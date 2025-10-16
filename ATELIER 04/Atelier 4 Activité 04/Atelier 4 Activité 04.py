b4b8e9369dca9756bd4a3264db983aa1962f7361
JdiraSiham <jdirasiham@gmail.com>
2025-10-16 14:00:49 +0100

    Add files via upload

---
+++ ATELIER 04/Atelier 4 Activité 04/Atelier 4 Activité 04.py
@@ -0,0 +1,11 @@
+#Réalisée par : JDIRA Siham 
+
+#Atelier 4 Activité 4:
+
+with open ("Table_de_multiplication.txt","w")as file :
+    for i in range (1,11):
+        file.write(f"La table de multiplication de {i:02d} : \n")
+        for j in range (1,11):
+            file.write(f"{i:02d} x {j:02d} = {i*j}\n")
+        file.write("\n")
+    file.close()
