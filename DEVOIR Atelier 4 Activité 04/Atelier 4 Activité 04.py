683596cc784b9c093aa24880d61463641bcbffc7
JdiraSiham <jdirasiham@gmail.com>
2025-10-19 14:04:44 +0100

    Update Atelier 4 Activité 04.py

--- DEVOIR Atelier 4 Activité 04/Atelier 4 Activité 04.py
+++ DEVOIR Atelier 4 Activité 04/Atelier 4 Activité 04.py
@@ -6,7 +6,8 @@ with open ("Table_de_multiplication.txt","w")as file :
     for i in range (1,11):
         file.write(f"La table de multiplication de {i:02d} : \n")
         for j in range (1,11):
-            file.write(f"{i:02d} x {j:02d} = {(i*j):02d}\n")
+            file.write(f"{i:02d} x {j:02d} = {i*j}\n")
         file.write("\n")
 
     file.close()
+
